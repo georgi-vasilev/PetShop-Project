79bdbc920c1771c96fdac3d11650f80a1cbbd48a
randomlettlers <35637497+randomlettlers@users.noreply.github.com>
2019-03-24 12:11:45 +0200

    Add files via upload

---
+++ PetShop/PetShop.Data/Models/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetShop.Data.Models
+{
+    public partial class Sales
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int EmployeeId { get; set; }
+        public int AnimalId { get; set; }
+        public DateTime? SaleDate { get; set; }
+
+        public virtual Animal Animal { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
